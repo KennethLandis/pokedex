7e3d47beee65e14d91b961f1e944ac1928ac93a3
Kenneth Landis <67128061+KennethLandis@users.noreply.github.com>
2021-03-26 17:56:55 -0500

    pokemon list
    Added script that would filter out unnecessary data and renders basic pokemon information into the dom.

--- index.js
+++ index.js
@@ -1,49 +1,68 @@
 'use strict'
 
-function getGen1() {
+// Gather the Pokemon from the first Generation with api call to pokeapi
+
+async function getGen1() {
     console.log('grabbing pokemon list');
-    let pokeList = 'https://pokeapi.co/api/v2/pokemon/?limit=1'
-    fetch(pokeList)
-        .then(response => response.json())
-        .then(responseJson => generateList(responseJson))    
+    const promises = [];
+    for (let i = 1; i < 40; i++) {
+        let pokeList = `https://pokeapi.co/api/v2/pokemon/${i}`
+        promises.push(fetch(pokeList).then(res => res.json()));
+    }
+    const results = await Promise.all(promises);
+    const pokemonList = pokemonIconFormat(results);
+    return pokemonList;
 };
 
+// calling pokeapi to get more details on the pokemon individually from the pokeapi
+
 function getPokemonInfo(currentPokemon) {
-    //console.log(currentPokemon);
     console.log('getting Pokemon details')
     let pokeInfo = `https://pokeapi.co/api/v2/pokemon/${currentPokemon}`;
     fetch(pokeInfo)
         .then(response => response.json())
-        .then(responseJson => displayPokemonElement(responseJson));
+        .then(responseJson => console.log(responseJson));
 };
 
-function generateList(responseJson) {
-    console.log('generating pokedex list');
-    console.log(responseJson);
-    for (let i = 0; i < responseJson.results.length; i ++) {
-        console.log(responseJson.results[i].name);
-        getPokemonInfo(responseJson.results[i].name);
-    };
+// Format the information on the Pokemon we got from the api into the values we need for Icons
+
+function pokemonIconFormat(results) {
+    var pokemonList = results.map(result => {
+        let pokemon = {}
+        pokemon['name'] = result.name
+        pokemon['id'] = result.id
+        pokemon['picture'] = result.sprites.front_default
+        pokemon['types'] = result.types.map(type => type.type.name).join(', ')
+        return pokemon;
+    });
+    return pokemonList
 };
 
-function generateListElement(currentPokemon) {
-    return `<li> <img src="${currentPokemon.sprites.front_default}"/> <p>${currentPokemon.id}</p>
-    <h3>${currentPokemon.name}</h3> <p>`
-};
+//Generates HTML String for displaying pokedex base Icon
+function generateIconHtml(currentPokemon) {
+    console.log(currentPokemon);
+    console.log('generating string');
+    return `<li> <img src="${currentPokemon.picture}"/> <h2>${currentPokemon.id}. ${currentPokemon.name}</h2>
+    <p>Types: ${currentPokemon.types}<p> </li>`
+}
 
-function displayPokemonElement(currentPokemon) {
-    var pokeString = generateListElement(currentPokemon);
-    $('.pokelist').html(pokeString);
-};
-
-function render() {
-    console.log('rendering page');
-    $(getGen1);
+//Function for Pushing html elements into the pokelist section in the DOM for displaying Icons
+function generatePokedexPage(pokemon) {
+    for (let i = 0; i < pokemon.length; i++) {
+        let pokeString = generateIconHtml(pokemon[i]);
+        console.log(pokeString);
+        $('.pokelist').append(pokeString);
+    }
+}
+   
+async function render() {
+    var pokemon = await getGen1();
+    generatePokedexPage(pokemon);
 };
 
 function filter() {
     console.log('filtering pokemon');
-}
+};
 
 function handleClicked() {
     console.log('handling clicked pokemon');
