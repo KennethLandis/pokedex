a699c072e0d16384cf485d7dbc815c9e32354ae4
Kenneth Landis <67128061+KennethLandis@users.noreply.github.com>
2021-03-26 22:56:45 -0500

    Filter Forms
    Added filter forms for name and type to html and javascript onkey triggers for filter functions that hide items that don't match the search query.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 async function getGen1() {
     console.log('grabbing pokemon list');
     const promises = [];
-    for (let i = 1; i < 40; i++) {
+    for (let i = 1; i < 152; i++) {
         let pokeList = `https://pokeapi.co/api/v2/pokemon/${i}`
         promises.push(fetch(pokeList).then(res => res.json()));
     }
@@ -39,28 +39,78 @@ function pokemonIconFormat(results) {
 };
 
 //Generates HTML String for displaying pokedex base Icon
+
 function generateIconHtml(currentPokemon) {
-    return `<div class ='icon'> <img src="${currentPokemon.picture}"/> <h2>${currentPokemon.id}. ${currentPokemon.name}</h2>
-    <p>Types: ${currentPokemon.types}<p> </div>`
+    return `<li class='icon'><img src="${currentPokemon.picture}"/> <h2>${currentPokemon.id}</h2> <h3><a href="#">${currentPokemon.name}</a></h3>
+    <p>Types: ${currentPokemon.types}</p></li>`
 }
 
 //Function for Pushing html elements into the pokelist section in the DOM for displaying Icons
+
 function generatePokedexPage(pokemon) {
     for (let i = 0; i < pokemon.length; i++) {
         let pokeString = generateIconHtml(pokemon[i]);
         $('.pokelist').append(pokeString);
     }
 }
-   
+
+// Function to generate the page in the Dom for user
+
 async function render() {
     var pokemon = await getGen1();
     generatePokedexPage(pokemon);
+    filterName();
+    filterType();
 };
 
-function filter() {
-    console.log('filtering pokemon');
+//function to handle filtering out pokemon that do not fit into search criteria
+//This is accomplished by gathering Elements from the doc and looping through list
+// to then show or hide items that fit or do not fit the search query.
+
+function filterName() {
+        // Declare variables
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById('species');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByTagName('li');
+        
+      
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+          } else {
+            li[i].style.display = "none";
+          }
+        }
 };
 
+//Same function as above with a new form targetting elements in the list by <p>
+function filterType() {
+        // Declare variables
+        var input, filter, ul, li, p, i, txtValue;
+        input = document.getElementById('type');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByTagName('li');
+            
+          
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+            p = li[i].getElementsByTagName("p")[0];
+            txtValue = p.textContent || p.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+            } else {
+            li[i].style.display = "none";
+            }
+        }
+};
+
+//function to handle clicking on pokemon icon for more information
 function handleClicked() {
     console.log('handling clicked pokemon');
 };
@@ -70,7 +120,6 @@ function handleClicked() {
 function handlePokedex() {
     $(render);
     $(handleClicked);
-    $(filter);
 };
 
 $(handlePokedex);
